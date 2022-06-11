53a47afa009f87a08c28a546432392013aae1370
JustAbby <abbyikoedem78@gmail.com>
2022-06-11 21:33:35 +0200

    DashBoard

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  <SideNav />
+</template>
+
+<script>
+// @ is an alias to /src
+import SideNav from '@/components/SideNavbar.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    SideNav,
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="d-flex">
+ <SideNav />
+  <div>
+        <NavBar />
+  <ContentSection />
+  </div>
+
+      
+      </div>
+    
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue';
+import ContentSection from '@/components/ContentsSection.vue';
+import SideNav from '@/components/SideNav.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavBar,
+    ContentSection,
+    SideNav,
+  }
+}
+</script>
+
+<style>
+
+</style>
